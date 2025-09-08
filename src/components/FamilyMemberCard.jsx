920a42d8c6aa2b613ac08737c26906ee6fd3430a
Magnus Overli <magnus.overli@gmail.com>
2025-09-08 11:32:22 +0200

    Fixing logic on buttons when deleting a family member

--- src/components/FamilyMemberCard.jsx
+++ src/components/FamilyMemberCard.jsx
@@ -151,7 +151,7 @@ const FamilyMemberCard = ({
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M15 5L5 15M5 5L15 15"
+                  d="M4 10L8 14L16 6"
                   stroke="currentColor"
                   strokeWidth="2"
                   strokeLinecap="round"
@@ -172,7 +172,7 @@ const FamilyMemberCard = ({
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M4 10L8 14L16 6"
+                  d="M15 5L5 15M5 5L15 15"
                   stroke="currentColor"
                   strokeWidth="2"
                   strokeLinecap="round"
